d4afcaf0f0dac0968f9b0850e5d138b844499a10
Ling930616 <lingling930616@gmail.com>
2024-05-21 20:23:46 +0800

    v1_朱芷伶

--- app/src/main/java/tw/edu/pu/csim/s1113422/myapplication/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1113422/myapplication/MainActivity.kt
@@ -1,16 +1,25 @@
 package tw.edu.pu.csim.s1113422.myapplication
 
+import android.graphics.fonts.Font
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import tw.edu.pu.csim.s1113422.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,11 +41,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    Image(
+        painter = painterResource(id = R.drawable.moyi),
+        contentDescription = "圖片",
+        alpha = 0.7f,
+        modifier = Modifier
+            .background(Color.Black)
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
